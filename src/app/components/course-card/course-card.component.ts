b36f36ef47d136d261ee4354f409c21958ea577b
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-09 15:10:41 -0800

    card edit

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../../services/course.service';
 import { Course } from '../../interfaces/interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-course-card',
   standalone: true,
   imports: [],
   templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css'
+  styleUrl: './course-card.component.css',
 })
 export class CourseCardComponent {
-  constructor(private courses:CourseService){}
+  constructor(private courses: CourseService, private http: HttpClient) {}
 
-  Courses:Course[] = this.courses.getCourses()
+  Courses: Course[] = this.courses.getCourses();
 
   startDate = new Date(2000, 0, 1);
 
-
-
+  ngOnInit() {
+    this.http.get('https://api.dictionaryapi.dev/api/v2/entries/en/hello').subscribe(response => console.log(response))
+  }
 }
